6ec7b294e5d1c3435c36c8e5258a7d47f50e7201
Dwaranir <yuriy.plakhin@gmail.com>
2023-01-08 09:00:03 +0600

    Lottie implemented

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
 import './index.scss'
+import './scripts/lottie.min.js' 
 
 
+
+// src="/assets/images/who-am-i-photo.png" 
+
+const who_am_i_animation = lottie.loadAnimation({
+    container: document.getElementById('who-am-i__animation-container'),
+    path: "/assets/animations/programmer.json",
+    render: "svg",
+    loop: true,
+    autoplay: true,
+    name: 'who-am-i__animation'}
+)
+
+const about_me_animation = lottie.loadAnimation({
+    container: document.getElementById('about-me__animation-container'),
+    path: "/assets/animations/programmer_dzen.json",
+    render: "svg",
+    loop: true,
+    autoplay: true,
+    name: 'who-am-i__animation'}
+)
